2df2a9d1efb8fcafba1ccb99b6d626668059efd7
xiajun.wang <xiajun.wang@galaxy-link.com>
2016-03-23 16:33:41 +0800

    添加资源,初始化

--- 百思不得姐/百思不得姐/AppDelegate.m
+++ 百思不得姐/百思不得姐/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,20 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    
+    //创建窗口
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    self.window.rootViewController = [[ViewController alloc] init];
+    
+    [self.window makeKeyAndVisible];
+    
+    
     return YES;
 }
 
