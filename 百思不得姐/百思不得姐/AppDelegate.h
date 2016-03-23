d96db5f528ed42bd41488f58c8b85c1f3b355264
xiajun.wang <xiajun.wang@galaxy-link.com>
2016-03-23 16:22:26 +0800

    first commit

---
+++ 百思不得姐/百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by ZZBelieve on 16/3/23.
+//  Copyright © 2016年 ZZBelieve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思不得姐/百思不得姐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  百思不得姐
+//
+//  Created by ZZBelieve on 16/3/23.
+//  Copyright © 2016年 ZZBelieve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
